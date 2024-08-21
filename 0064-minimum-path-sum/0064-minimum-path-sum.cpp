2295b371327016967e8c092440db150ece54e201
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-08-21 19:57:49 +0530

    Time: 7 ms (60.87%), Space: 13.4 MB (13.24%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -18,6 +18,22 @@ public:
         int row = grid.size();
         int col = grid[0].size();
         vector<vector<int>> dp(row, vector<int>(col, -1));
+        vector<int> prev(col,0);
+        for(int i=0; i<row; i++){
+            vector<int> curr(col,0);
+            for(int j=0; j<col; j++){
+                if(i == 0 && j == 0) curr[j]=grid[0][0];
+                else{   
+                    if(i == 0) curr[j] = grid[i][j]+curr[j-1];
+                    else if(j == 0) curr[j] = grid[i][j]+prev[j];
+                    else{
+                        curr[j] = min(grid[i][j]+prev[j],grid[i][j]+curr[j-1]);
+                    }
+                }
+            }
+            prev = curr;
+        }
+        return prev[col-1];
         return f(row-1,col-1,grid,dp);
     }
 };

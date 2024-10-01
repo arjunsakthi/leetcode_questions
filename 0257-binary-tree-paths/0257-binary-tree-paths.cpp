25e5981c8da88259eb16cb5379db1d8b17c6dbc4
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-10-01 19:09:55 +0530

    Time: 4 ms (50.6%), Space: 15.6 MB (98.69%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> paths;
+        vector<string> ans;
+        st.push(root);
+        paths.push_back(root->val);
+        TreeNode *temp = root->left;
+        if(root->left == NULL && root->right == NULL)   return {to_string(root->val)};
+        while(!st.empty()){
+            if(temp != NULL){
+                st.push(temp);
+                paths.push_back(temp->val);
+                temp = temp->left;
+            }
+            else{
+                TreeNode *ktemp = st.top()->right;
+                if(ktemp == NULL){
+                    string kk="";
+                    if(st.top()->left == NULL){
+                        for(int i=0; i<paths.size()-1;i++){
+                        kk += to_string(paths[i]);
+                        kk+= "->";
+                    }
+                    kk+= to_string(paths[paths.size()-1]);
+                    ans.push_back(kk);
+                    }
+                    
+                    ktemp = st.top();
+                    st.pop();
+                    paths.pop_back();
+                    while(!st.empty() && ktemp == st.top()->right){
+                        ktemp = st.top();
+                        st.pop();
+                        paths.pop_back();
+                    }
+                }
+                else{
+                    temp = st.top()->right;
+                }
+            }
+        }
+        return ans;
+    }
+};

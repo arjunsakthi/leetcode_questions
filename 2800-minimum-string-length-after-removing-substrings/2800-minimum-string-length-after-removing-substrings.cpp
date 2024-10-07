77d47b8011fdd89c70352b27ae7ae48dd75cdd20
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-10-07 07:37:35 +0530

    Time: 115 ms (5.05%), Space: 10 MB (93.92%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int cnt = 0;
+        int i=0;
+        int k = s.size()-1;
+        while(i<k){
+            cout << k <<" ";
+            if(s[i] == 'A' && s[i+1] == 'B'){
+                s.erase(s.begin()+i,s.begin()+i+2);
+                i=0;
+                 k = s.size()-1;
+                continue;
+            }
+            if(s[i] == 'C' && s[i+1] == 'D'){
+                 s.erase(s.begin()+i,s.begin()+i+2);
+                 i=0;
+                  k = s.size()-1;
+                 continue;
+            }
+            i++;
+        }
+        return s.size();
+    }
+};

5d7a1aa7ef1c2335dc841e69746f558d8256ef98
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-10-07 12:04:44 +0530

    Time: 3 ms (59.09%), Space: 9 MB (67.55%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int flag = 0; // Flag to indicate if the number is negative
-        int i = 0;
-        
-        // Skip leading spaces
-        while (i < s.size() && s[i] == ' ') {
+        int flag = 0;
+        int i=0;
+        while(i<s.size() && s[i] == ' '){
             i++;
         }
-
-        // Handle sign characters
-        if (i < s.size() && s[i] == '-') {
-            flag = 1;
-            i++;
-        } else if (i < s.size() && s[i] == '+') {
+        // if(i+1 == s.size()) return 0;
+        if(s[i] == '-'){
+            flag=1;
             i++;
         }
-
+        else if(s[i] == '+'){
+            i++;
+        }
         int sum = 0;
-        while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
-            int digit = s[i] - '0';
+        while(i<s.size()){
+            if(s[i] >= '0' && s[i] <= '9'){
+                int digit = s[i] - '0';
 
             // Check for overflow before updating sum
             if (sum > (INT_MAX - digit) / 10) {
@@ -27,9 +25,12 @@ public:
             }
 
             sum = sum * 10 + digit;
+            }
+            else{
+                break;
+            }
             i++;
         }
-
-        return flag ? -sum : sum;
+        return flag == 1 ?-sum:sum;
     }
 };

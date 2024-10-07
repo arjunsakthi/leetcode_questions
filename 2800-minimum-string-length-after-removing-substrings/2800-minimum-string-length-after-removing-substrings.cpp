0e76e06aeaf827dcb1680153a9b6197fc7a79fc3
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-10-07 07:45:21 +0530

    Time: 7 ms (51.96%), Space: 10.1 MB (93.92%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -5,15 +5,16 @@ public:
         int i=0;
         int k = s.size()-1;
         while(i<k){
-            cout << k <<" ";
             if(s[i] == 'A' && s[i+1] == 'B'){
                 s.erase(s.begin()+i,s.begin()+i+2);
-                i=0;
+                if(i != 0) i--;
+                k = s.size()-1;
                 continue;
             }
             if(s[i] == 'C' && s[i+1] == 'D'){
                  s.erase(s.begin()+i,s.begin()+i+2);
-                 i=0;
+                 if(i!=0)   i--;
+                 k = s.size()-1;
                  continue;
             }
             i++;

5eff74208455bc9227d46610fbbd0b14eae2a913
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-10-11 14:13:12 +0530

    Time: 3 ms (90.36%), Space: 8.4 MB (94.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<char> chra(128);
+        vector<char> chra(128,0);
+        vector<char> matched(128,0);
 
         for(int i=0 ; i<s.size(); i++){
             
             if(chra[s[i]] == 0){
-                for(int j=0; j<chra.size();j++){
-                    if(chra[j] == t[i]) return false;
-                }
+                    if(matched[t[i]] != 0) return false;
+                
                 chra[s[i]]  = t[i];
+                matched[t[i]] = s[i];
             }
             else{
                 if(chra[s[i]] != t[i]) return false;

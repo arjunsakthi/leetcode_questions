f13817fc212f59cb1a70f26f41f7893600ee7350
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-11-02 13:58:57 +0530

    Time: 67 ms (28.11%), Space: 16.7 MB (56.91%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
     bool isAllStars(string &S1, int i) {
-        // Check if the pattern up to index i contains only '*'
-        for (int j = 1; j <= i; j++) {
-            if (S1[j - 1] != '*')
-                return false;
-        }
-        return true;
+    // S1 is taken in 1-based indexing
+    for (int j = 1; j <= i; j++) {
+        if (S1[j - 1] != '*')
+            return false;
     }
+    return true;
+}
+    bool rec(int i, int j, string s, string p){
+        if(i<0 && j<0)  return true;
+        else if(i <0 || j<0)    return false;
 
-    bool isMatch(string s, string p) {
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-
-        // Base case: Both s and p are empty
-        dp[0][0] = true;
-
-        // Initialize first row for cases where s is empty
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = isAllStars(p, j);  // Only '*' can match an empty string
+        if(p[j] == '?'){
+            return rec(i-1,j-1,s,p);
         }
+        else if(p[j] == '*'){
 
-        // Fill the dp table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p[j - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                }
-                else {
-                    dp[i][j] = (s[i - 1] == p[j - 1]) && dp[i - 1][j - 1];
-                }
+                return rec(i-1,j,s,p) || rec(i,j-1,s,p) || rec(i-1,j-1,s,p);
+        }
+        else{
+            if(s[i] != p[j])    return false;
+            else    return rec(i-1,j-1,s,p);
+        }
+        return false;
+    }
+    bool isMatch(string s, string p) {
+        // return rec(s.size()-1, p.size()-1, s, p);
+        vector<vector<bool>> dp(s.size()+1, vector<bool>(p.size()+1,0));
+        dp[0][0] = true;
+        for (int i = 1; i <= p.size(); i++) {
+            dp[0][i] = isAllStars(p, i);
+        }
+        for(int i=1; i<=s.size(); i++){
+            for(int j=1; j<=p.size(); j++){
+                if(p[j-1] == '?'){
+            dp[i][j] = dp[i-1][j-1];
+        }
+        else if(p[j-1] == '*'){
+
+                dp[i][j] = dp[i-1][j] || dp[i][j-1];
+        }
+        else{
+            dp[i][j] = (s[i - 1] == p[j - 1]) && dp[i - 1][j - 1];
+        }
             }
         }
-
-        return dp[m][n];
+        return dp[s.size()][p.size()];
     }
 };

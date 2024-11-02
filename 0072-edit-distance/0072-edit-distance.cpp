dd48dd3a672a7271c2bea7061e2dad95a59abdf1
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-11-02 13:06:45 +0530

    Time: 71 ms (10.64%), Space: 82.7 MB (7.4%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,13 +3,14 @@ public:
     int rec(int i, int j, string word1, string word2,vector<vector<int>> &dp){
         if(j<0 || i<0)  return max(i+1,j+1);
         if(dp[i][j] != -1)  return dp[i][j];
-        int insert = 1 + rec(i-1,j,word1,word2, dp);
-        int del = min(insert, 1 + rec(i,j-1,word1,word2, dp));
-        int replace = min(del,1 + rec(i-1,j-1,word1,word2, dp));
+        
         if(word1[i] == word2[j]){
-            return dp[i][j] = min(rec(i-1,j-1,word1,word2, dp),replace);
+            return dp[i][j] = rec(i-1,j-1,word1,word2, dp);
         }
         else{
+            int insert = 1 + rec(i-1,j,word1,word2, dp);
+            int del = min(insert, 1 + rec(i,j-1,word1,word2, dp));
+            int replace = min(del,1 + rec(i-1,j-1,word1,word2, dp));
             return dp[i][j] = replace;
         }
     }

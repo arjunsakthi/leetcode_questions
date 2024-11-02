7127075fc5636fd937c8ab83414d5bc56420d354
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-11-02 13:20:06 +0530

    Time: 4 ms (77.66%), Space: 12.8 MB (63.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -15,7 +15,29 @@ public:
         }
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
-        return rec(word1.size()-1, word2.size()-1,word1, word2, dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for(int i=0; i<=word1.size(); i++){
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=word2.size(); j++){
+            dp[0][j] = j;
+        }
+        // return rec(word1.size()-1, word2.size()-1,word1, word2, dp);
+        if(word1.size() == 0)   return word2.size();
+        if(word2.size() == 0)   return word1.size();
+        for(int i=1; i<=word1.size(); i++){
+            for(int j=1; j<= word2.size(); j++){
+                if(word1[i-1] == word2[j-1]){
+            dp[i][j] = dp[i-1][j-1];
+        }
+        else{
+            int insert = 1 + dp[i][j-1];
+            int del = min(insert, 1 +dp[i-1][j]);
+            int replace = min(del,1 + dp[i-1][j-1]);
+            dp[i][j] = replace;
+        }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };

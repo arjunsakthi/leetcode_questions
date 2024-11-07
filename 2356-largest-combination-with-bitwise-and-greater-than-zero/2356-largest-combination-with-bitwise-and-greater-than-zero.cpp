7e4c7dfa601301c5320597186577480e8920622d
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-11-07 15:49:25 +0530

    Time: 43 ms (19.4%), Space: 60.1 MB (74.4%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        map<int,int> mpp;
-        for(int i=0; i<candidates.size(); i++){
-            for(int j=0; j<32; j++){
-                if(candidates[i]&(1<<j))  mpp[j]++;
-            }
-        }
         int maxi = 0;
-        for(auto k : mpp){
-            maxi = max(k.second,maxi);
+            for(int j=0; j<32; j++){
+                int count = 0;
+                for(int i=0; i<candidates.size(); i++){
+                    if(candidates[i]&(1<<j))  count++;
+            }
+            maxi = max(count,maxi);
         }
         return maxi;
     }

92095fef1bcad8972ba695b0c494b77bc7d04513
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-11-10 12:19:00 +0530

    Time: 273 ms (26.37%), Space: 274.4 MB (6.96%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
+    void update(vector<int> &bitset, int n, int op){
+        for(int i=0; i<32; i++){
+            if(n&(1<<i)){
+                bitset[i] += op;
+            }
+        }
+    }
+    bool check(vector<int> bitset, int k){
+        int n  =0;
+        for(int i=0; i<32; i++){
+            if(bitset[i]){
+                n |= (1<<i);
+            }
+        }
+        return k<=n;
+    }
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int sum = 0;
         int mini = INT_MAX;
         int i=0;
-        while(i<nums.size()){
-            sum = nums[i] | sum;
-            if(sum >=k){
-                int temp= 0;
-                int j=i;
-                while(j >=0 && temp < k){
-                    temp = temp|nums[j];
-                    j--;
+        int left = 0; 
+        int right = 0;
+        vector<int> bitset(32,0);
+        while(right < nums.size()){
+            update(bitset, nums[right],1);
+            if(check(bitset, k)){
+                while(left <= right && check(bitset,k)){
+                    mini = min(mini, right-left+1);
+                    update(bitset, nums[left],-1);
+                    left++;
                 }
-                mini = min(mini, i-j);
-                sum = 0;
-                i=j+1;
             }
-            i++;
+            right++;
         }
         return k==0 ? 1 : (mini == INT_MAX)? -1:mini;
     }

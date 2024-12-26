72fdb688ab812fac05a168dbae6aa02b14e2282e
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2024-12-26 23:44:14 +0530

    Time: 21 ms (74.34%), Space: 14.1 MB (15.75%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
+    int minC(int low, int high, vector<int> &cuts){
+        if(high < low) return 0;
+        int mini = INT_MAX;
+        for(int i=low; i<=high; i++){
+            int minc = minC(low, i-1,cuts) + minC(i+1,high,cuts) + cuts[high+1]-cuts[low-1];
+            mini = min(mini, minc);
+        }
+        return mini;
+    }
+
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
 
         sort(cuts.begin(), cuts.end());
-
+        // return minC(1,cuts.size()-2,cuts);
         int m = cuts.size();
         vector<vector<int>> dp(m, vector<int>(m, 0));
 

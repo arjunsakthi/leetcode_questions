58e8aa8ef78432077be6081ab2326c66cf3b0b50
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-01-07 01:04:17 +0530

    Time: 24 ms (64.18%), Space: 79.4 MB (87.42%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> preSmaller(heights.size());
-        vector<int> nextSmaller(heights.size());
         stack<int> st;
-        for(int i=0; i<heights.size();i++){
-            if(st.empty())  preSmaller[i] = -1;
-            else{
-                while(!st.empty() && heights[st.top()] >= heights[i]){
-                    st.pop();
-                }
-                if(st.empty())  preSmaller[i] = -1;
-                else{
-                    preSmaller[i] = st.top();
-                }
+        int maxA = 0;
+        int n = heights.size();
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+                maxA = max(maxA, width * height);
             }
             st.push(i);
         }
-        while(!st.empty()){
-            st.pop();
-        }
-         for(int i=heights.size()-1; i>=0;i--){
-            if(st.empty())  nextSmaller[i] = heights.size();
-            else{
-                while(!st.empty() && heights[st.top()] >= heights[i]){
-                    st.pop();
-                }
-                if(st.empty())  nextSmaller[i] = heights.size();
-                else{
-                    nextSmaller[i] = st.top();
-                }
-            }
-            st.push(i);
-        }
-        int maxi = 0;
-        for(int i=0; i<heights.size(); i++){
-            maxi = max(maxi, (nextSmaller[i] - preSmaller[i] - 1)*heights[i]);
-        }
-        return maxi;
+        return maxA;
     }
 };

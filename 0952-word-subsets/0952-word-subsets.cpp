fb92e1c3543cfa7e13208e82a268b3b1a63d2d10
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-01-10 15:20:18 +0530

    Time: 59 ms (57.53%), Space: 106.9 MB (42.47%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<int> w2(26, 0);
+        vector<string> ans;
+        for (int i = 0; i < words2.size(); i++) {
+            vector<int> hash(26, 0);
+            string temp = words2[i];
+            for (int j = 0; j < temp.size(); j++) {
+                hash[temp[j] - 'a']++;
+            }
+            for (int j = 0; j < 26; j++) {
+                w2[j] = max(w2[j], hash[j]);
+            }
+        }
+        for (int i = 0; i < words1.size(); i++) {
+            string temp = words1[i];
+            vector<int> hash(26, 0);
+            int flag = 0; for (int j = 0; j < temp.size(); j++) {
+                hash[temp[j] - 'a']++;
+            }
+            for (int j = 0; j < 26; j++) {
+                if (hash[j] < w2[j]) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (!flag) {
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};

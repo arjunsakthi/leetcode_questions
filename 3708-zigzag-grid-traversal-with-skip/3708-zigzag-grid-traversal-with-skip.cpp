ae18b4284b04cdef9822cba7d249473365f62acf
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-01-12 18:57:23 +0530

    Time: 0 ms (100%), Space: 31.9 MB (30%) - LeetHub

---
+++ 3708-zigzag-grid-traversal-with-skip/3708-zigzag-grid-traversal-with-skip.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> zigzagTraversal(vector<vector<int>>& grid) {
+        vector<int> ans;
+        for(int i=0; i<grid.size(); i++){
+            if(i%2 == 0){
+                 for(int j=0; j<grid[0].size(); j++){
+                    if((j+i)%2 == 0){
+                        ans.push_back(grid[i][j]);
+                    }
+                }
+            }
+            else{
+                for(int j=grid[0].size()-1; j>=0; j--){
+                    if((j+i)%2 == 0){
+                        ans.push_back(grid[i][j]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};

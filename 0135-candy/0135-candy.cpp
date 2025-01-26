e6e9f5bf09d97aef3561a09e2101e9d0bf24e468
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-01-26 23:11:06 +0530

    Time: 0 ms (100%), Space: 22.1 MB (53.81%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> choco(ratings.size(),1);
-        for(int i=1; i<ratings.size(); i++){
-            if(ratings[i] > ratings[i-1]){
-                choco[i] = choco[i-1] + 1;
+        // slope concept
+        int i=1,n=ratings.size();
+        int sum = 1; 
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int decr = 1;
+            while(i < n && ratings[i] < ratings[i-1]){
+                
+                sum += decr;
+                decr++;
+                i++;
+            }
+            if(decr > peak){
+                sum += decr - peak;
             }
         }
-        int count = 0;
-        for(int i=ratings.size()-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1] && choco[i] <= choco[i+1]){
-                choco[i] = choco[i+1]+1;
-            }           
-        }
-        int sum = accumulate(choco.begin(), choco.end(),0);
         return sum;
+        // vector<int> choco(ratings.size(),1);
+        // for(int i=1; i<ratings.size(); i++){
+        //     if(ratings[i] > ratings[i-1]){
+        //         choco[i] = choco[i-1] + 1;
+        //     }
+        // }
+        // int count = 0;
+        // for(int i=ratings.size()-2; i>=0; i--){
+        //     if(ratings[i] > ratings[i+1] && choco[i] <= choco[i+1]){
+        //         choco[i] = choco[i+1]+1;
+        //     }           
+        // }
+        // int sum = accumulate(choco.begin(), choco.end(),0);
+        // return sum;
     }
 };

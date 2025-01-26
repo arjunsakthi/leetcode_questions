27899cb50a92bd3ef59d09477acd556861481394
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-01-26 22:36:21 +0530

    Time: 1 ms (36.56%), Space: 22.7 MB (36.39%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> hash(ratings.size(), 0);
         vector<int> choco(ratings.size(),1);
         for(int i=1; i<ratings.size(); i++){
             if(ratings[i] > ratings[i-1]){
                 choco[i] = choco[i-1] + 1;
-            }
-            else if(ratings[i] == ratings[i-1]){
-                continue;
-            }
-            else{
-                if(choco[i] == choco[i-1])
-                    hash[i-1] += 1;
             }
         }
         int count = 0;

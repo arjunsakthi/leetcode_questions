6f34dc9043ab20e8526601c39e8b52cb4c25f183
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-01-27 22:03:27 +0530

    Time: 0 ms (100%), Space: 21.8 MB (48.63%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -3,21 +3,20 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
         int i=0;
-       auto front = lower_bound(intervals.begin(), intervals.end(), newInterval[0],[](vector<int> &element, int value){
-        return element[0] < value;
-       });
-       intervals.insert(front, newInterval);
-       vector<int> prev = intervals[0];
-       for(int i=1; i<intervals.size(); i++){
-        if(prev[1] < intervals[i][0]){
-            ans.push_back(prev);
-            prev = intervals[i];
-        }
-        else{
-            prev[1] = max(intervals[i][1],prev[1]);
-        }
-       }
-       ans.push_back(prev);
+            while(i < intervals.size() && intervals[i][1] < newInterval[0]){
+                ans.push_back(intervals[i]);
+                i++;
+            }
+            while(i < intervals.size() && intervals[i][0] <= newInterval[1]){
+                newInterval[1] = max(intervals[i][1], newInterval[1]);
+                newInterval[0] = min(intervals[i][0],newInterval[0]);
+                i++;
+            }
+            ans.push_back(newInterval);
+            while(i < intervals.size()){
+                ans.push_back(intervals[i]);
+                i++;
+            }
         return ans;
     }
 };

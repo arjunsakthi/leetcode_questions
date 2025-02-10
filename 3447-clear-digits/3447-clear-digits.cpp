2d282749d41ddf8d2a3f306e9d9a443153265130
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-10 18:42:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.37%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans = "";
+        for(auto ch : s){
+            if(ch>='0' && ch <='9'){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(ch);
+            }
+        }
+        return ans;
+    }
+};

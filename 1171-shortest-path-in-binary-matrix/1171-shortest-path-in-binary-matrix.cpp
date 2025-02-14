f27bff53b342e06dbbba88409e913cdad4da41b1
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-14 23:08:21 +0530

    Time: 28 ms (41.75%), Space: 23.3 MB (96.28%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,7 +3,7 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         if(grid[0][0] == 1) return -1;
         // shortest path so dijkstra 
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         vector<vector<int>> distance = grid;
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
@@ -11,20 +11,19 @@ public:
             }
         }
         distance[0][0] = 1;
-        pq.push({1,{0,0}});
+        pq.push({0,0});
         while(!pq.empty()){
-            int dis = pq.top().first;
+            int dis = distance[pq.top().first][pq.top().second];
             
-            pair<int,int> cor = pq.top().second;
-            int row = cor.first;
-            int col = cor.second;
+            int row = pq.top().first;
+            int col = pq.top().second;
             pq.pop();
             for(int i=-1; i<=1; i++){
                 for(int j=-1; j<=1; j++){
                     if(i==0 && j == 0)  continue;
                     if(  row+i < grid.size() && col+j < grid[0].size() && row+i >=0 && col+j >= 0 && grid[row+i][col+j] != 1 && (distance[row+i][col+j] == -1 || distance[row+i][col+j] > dis+1)){
                         distance[row+i][col+j] = dis+1;
-                        pq.push({dis+1, {row+i,col+j}});
+                        pq.push({row+i,col+j});
                         if(row+i == grid.size()-1 && col+j == grid[0].size()-1) return distance[grid.size()-1][grid[0].size()-1];
                     }
                 }

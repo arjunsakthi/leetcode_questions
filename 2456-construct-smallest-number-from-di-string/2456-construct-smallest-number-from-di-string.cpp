e0b774f27c2eb3c46937ef45858f9659d118c307
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-18 16:49:25 +0530

    Time: 0 ms (100%), Space: 8 MB (57.81%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string smallestNumber(string pattern) {
-       pattern = 'I'+pattern;
-        int incI = 1;
-        int decI = 1;
-        int j = 0;
-        string ans = "";
-        while(j < pattern.size()){
-            int inc=0;
-            int dec=0;
-            while(j<pattern.size() && pattern[j] == 'I')inc++,j++;
-            while(j<pattern.size() && pattern[j] == 'D')dec++,j++;
-            while(inc > 1) ans.push_back('0'+incI),incI++,inc--;
-             incI += dec; ans.push_back('0'+incI);inc--;
-             decI = incI-1;
-             incI++;
-            while(dec > 0) ans.push_back('0'+decI),decI--,dec--;
+        int n = pattern.length();
+        string result = "";
+        int stack[n + 1];
+        int index = 0;
+
+        for (int i = 0; i <= n; i++) {
+            stack[index++] = i + 1;
+
+            if (i == n || pattern[i] == 'I') {
+                while (index > 0) {
+                    result += to_string(stack[--index]);
+                }
+            }
         }
-        return ans;
-    }  
+
+        return result;
+    }
 };

460aa64f9dd063ef159f60469daa12f7cb3d1d28
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-18 16:47:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.43%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-    vector<int> ans;
-
-    string vectorToString(){
-        string temp = "";
-        for(auto i:this->ans){
-            temp.push_back(i+'0');
-        }
-        return temp;
-    }
-
     string smallestNumber(string pattern) {
-        vector<int> used(10,0);
-        vector<int> temp(pattern.size()+1,-1);
-        for(int i=1; i<10; i++){
-            temp[0] = i;
-            used[i] = 1;
-            if(rec(pattern, used, 1,temp))  break;
-            temp[0] = -1;
-            used[i] = 0;
+       pattern = 'I'+pattern;
+        int incI = 1;
+        int decI = 1;
+        int j = 0;
+        string ans = "";
+        while(j < pattern.size()){
+            int inc=0;
+            int dec=0;
+            while(j<pattern.size() && pattern[j] == 'I')inc++,j++;
+            while(j<pattern.size() && pattern[j] == 'D')dec++,j++;
+            while(inc > 1) ans.push_back('0'+incI),incI++,inc--;
+             incI += dec; ans.push_back('0'+incI);inc--;
+             decI = incI-1;
+             incI++;
+            while(dec > 0) ans.push_back('0'+decI),decI--,dec--;
         }
-        return vectorToString();
-    }
-private:
-    bool rec(string pattern,vector<int> &used, int ind, vector<int> &temp){
-        if(ind - 1 == pattern.size()){
-            ans = temp;
-            return true;
-        }
-
-        for(int i=1; i<10; i++){
-            if(used[i]) continue;
-            else if((pattern[ind-1] == 'I' && temp[ind-1] < i) || (pattern[ind-1] == 'D' && temp[ind-1] > i)){
-                temp[ind] = i;
-                used[i] = 1;
-                if(rec(pattern, used, ind+1, temp))   return true;
-                temp[ind] = -1;
-                used[i] = 0;
-            }
-        }
-        return false;
-    }
+        return ans;
+    }  
 };

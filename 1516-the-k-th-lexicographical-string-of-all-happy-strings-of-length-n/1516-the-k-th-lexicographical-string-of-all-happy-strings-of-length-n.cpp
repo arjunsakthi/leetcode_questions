98bd73a1f79b0d6caad50391f4ad40cc3dc76e38
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-19 16:08:40 +0530

    Time: 3 ms (74.09%), Space: 7.8 MB (99.21%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -4,28 +4,28 @@ public:
     string getHappyString(int n, int k) {
         int num = 0;
         string temp = "";
-        rec(n, k, num, temp);
-        return ans;
+        bool hello = rec(n, k, num, temp);
+        return hello ? ans: "";
     }
 
 private:
-    void rec(int n, int& k, int& num, string& temp) {
-
+    bool rec(int n, int& k, int& num, string& temp) {
         if (n == 0) {
             num++;
             if (num == k) {
                 ans = temp;
+                return true;
             }
-            return;
+            return false;
         }
         for (int i = 0; i < 3; i++) {
             char ch = 'a' + i;
             if(!temp.empty() && temp.back() == ch)  continue;
             
             temp.push_back(ch);
-            rec(n - 1, k, num, temp);
+            if(rec(n - 1, k, num, temp)) return true;
             temp.pop_back();
         }
-        return;
+        return false;
     }
 };

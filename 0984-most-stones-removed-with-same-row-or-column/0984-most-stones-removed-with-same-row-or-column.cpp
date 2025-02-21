d54134de7f8bc14ff69c3163edbcbfa5e23899fd
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-21 22:20:39 +0530

    Time: 1 ms (98%), Space: 24.6 MB (40.12%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,51 +1,32 @@
 class Solution {
-public:
-     vector<int> parent, size;
-    int pUltimate(int ele) {
-        return parent[ele] = ele == parent[ele] ? ele : pUltimate(parent[ele]);
+private:
+    int connectedComponentCount = 0;
+
+    int findRepresentative(int element, vector<int>& setRepresentatives) {
+        if (setRepresentatives[element] == 0) {
+            setRepresentatives[element] = element;
+            connectedComponentCount++;
+        }
+        return setRepresentatives[element] == element ? element : 
+               (setRepresentatives[element] = findRepresentative(setRepresentatives[element], setRepresentatives));
     }
 
-    // union by size
-    void Union(int ele1, int ele2) {
-        
-        int par1 = pUltimate(ele1);
-        int par2 = pUltimate(ele2);
-        if(par1 == par2)    return ;
-        if (size[par1] > size[par2]) {
-            size[par1] += size[par2];
-            parent[par2] = par1;
-        } else {
-            size[par2] += size[par1];
-            parent[par1] = par2;
+    void mergeComponents(int elementA, int elementB, vector<int>& setRepresentatives) {
+        int repA = findRepresentative(elementA, setRepresentatives);
+        int repB = findRepresentative(elementB, setRepresentatives);
+        if (repA != repB) {
+            setRepresentatives[repB] = repA;
+            connectedComponentCount--;
         }
     }
-    int removeStones(vector<vector<int>>& stones) {
-        parent.resize(stones.size());
-        size.resize(stones.size(),1);
-        map<int,vector<int>> row, col;
-        for(int i=0; i<stones.size(); i++){
-            parent[i] = i;
-            row[stones[i][0]].push_back(i);
-            col[stones[i][1]].push_back(i);
+
+public:
+    int removeStones(vector<vector<int>>& stonePositions) {
+        vector<int> setRepresentatives(20003, 0);
+        for (const auto& stonePosition : stonePositions) {
+            mergeComponents(stonePosition[0] + 1, stonePosition[1] + 10002, setRepresentatives);
         }
-        for(auto &rw : row){
-            vector<int> r = rw.second;
-            if(r.size() == 1)   continue;
-            for(int i=1; i<r.size(); i++){
-                Union(r[i-1],r[i]);
-            }
-        }
-        for(auto &cl : col){
-            vector<int> c = cl.second;
-            if(c.size() == 1)   continue;
-            for(int i=1; i<c.size(); i++){
-                Union(c[i-1],c[i]);
-            }
-        }
-        int count = 0;
-        for(int i=0; i<stones.size(); i++){
-            if(pUltimate(i) == i)   count++;
-        }
-        return stones.size()-count;
+        return stonePositions.size() - connectedComponentCount;
     }
 };
+

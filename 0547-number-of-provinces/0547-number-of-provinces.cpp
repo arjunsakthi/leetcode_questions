a4cf6bf9608d792d4630be7204c3d77fac0466ce
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-21 19:50:03 +0530

    Time: 270 ms (5.03%), Space: 24.3 MB (5.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
-
-    void dfs(int ind, vector<int> &visited, unordered_map<int,vector<int>> &adj){
-        visited[ind] = 1;
-        for(auto i : adj[ind]){
-            if(!visited[i]){
-                dfs(i, visited, adj);
-            }
-        }
-        return;
+    vector<int> parent, size;
+    int pUltimate(int ele) {
+        return parent[ele] = ele == parent[ele] ? ele : pUltimate(parent[ele]);
     }
 
+    // union by size
+    void Union(int ele1, int ele2) {
+        
+        int par1 = pUltimate(ele1);
+        int par2 = pUltimate(ele2);
+        cout << ele1 << " " << par1 << " " << ele2 << " " << par2 << endl;
+        if(par1 == par2)    return ;
+        if (size[par1] > size[par2]) {
+            size[par1] += size[par2];
+            parent[par2] = par1;
+        } else {
+            size[par2] += size[par1];
+            parent[par1] = par2;
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        unordered_map<int,vector<int>> adj;
-        for(int i=0; i<isConnected.size(); i++){
-            for(int j=0; j<isConnected[0].size(); j++){
-                if(i != j && isConnected[i][j]){
-                    adj[i].push_back(j);
+        parent.resize(isConnected.size());
+        size.resize(isConnected.size(),1);
+        vector<vector<int>> adj;
+        for (int i = 0; i < isConnected.size(); i++) {
+            parent[i]=i;
+            for (int j = 0; j < isConnected[0].size(); j++) {
+                if (i != j && isConnected[i][j]) {
+                    adj.push_back({i,j});
                 }
             }
         }
-        vector<int> visited(isConnected.size(), 0);
-        int provisionCount = 0;
-        for(int i=0; i<isConnected.size(); i++){
-            if(!visited[i] ){
-               dfs(i, visited, adj);
-                provisionCount++;
-            }
+        for(auto &i : adj){
+            Union(i[0],i[1]);
         }
-        return provisionCount;
+        set<int> st;
+        for(int i=0; i<isConnected.size(); i++){
+            st.insert(pUltimate(i));
+        }
+        return st.size();
+       
     }
 };

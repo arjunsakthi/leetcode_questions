87427569a28a88d88c2d9d71cd345845123f590e
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-02-26 22:03:59 +0530

    Time: 1383 ms (5.02%), Space: 170.3 MB (78.72%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
-    int timer = 1;
-    void dfs(int node, int parent, vector<int> &vis,
-             vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges) {
-        vis[node] = 1;
-        tin[node] = low[node] = timer++;
-        for (auto it : adj[node]) {
-            if (it == parent) continue;
-            if (!vis[it]) {
-                dfs(it, node, vis, adj, tin, low, bridges);
-                low[node] = min(low[it], low[node]);
-                if (low[it] > tin[node]) {
-                    bridges.push_back({it, node});
+    int ind = 1;
+    void dfs(int n, vector<vector<int>> &adj, vector<int> &visited, int parent, vector<int> &tIn, vector<int> &mTin, vector<vector<int>> &ans){
+        for(auto i : adj[n]){
+            if(i != parent){
+                if(!visited[i]){
+                    tIn[i] = ind;
+                    mTin[i] = ind;
+                    ind++;
+                    visited[i]=1;
+                    dfs(i,adj,visited,n,tIn,mTin,ans);
+                    if(mTin[i] <= tIn[n]){
+                        // here min is requ
+                        mTin[n] = min(mTin[i],mTin[n]);
+                    }
+                    else{
+                        cout << mTin[i] << " " << mTin[n];
+                        ans.push_back({i,n});
+                    }
                 }
-            } else {
-                low[node] = min(low[node], low[it]);
+                else if(visited[i]){
+                    cout << n << i << endl;
+                    mTin[n] = min(mTin[i],mTin[n]);
+                }
             }
         }
     }
 
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<int> adj[n];
-        for (auto it : connections) {
-            int u = it[0], v = it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        vector<int> vis(n, 0), tin(n), low(n);
-        vector<vector<int>> bridges;
-        dfs(0, -1, vis, adj, tin.data(), low.data(), bridges);
-        return bridges;
+                vector<vector<int>> adj(n);
+                for(auto &i : connections){
+                    adj[i[0]].push_back(i[1]); 
+                    adj[i[1]].push_back(i[0]); 
+                }
+                vector<int> visited(n,0);
+                visited[0]=1;
+                int parent = 0;
+                vector<int> tIn(n);
+                vector<int> mTin(n);
+                tIn[0] = 0;
+                mTin[0] = 0;
+                vector<vector<int>> ans;
+                dfs(0,adj,visited,parent,tIn,mTin, ans);
+                return ans;
     }
 };

695edae4b204a4f05e003474074daa9ebc9f835a
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-01 23:42:36 +0530

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -7,14 +7,16 @@ public:
                 nums[i + 1] = 0;
             }
         }
-        vector<int> ans;
-        int c=0;
+        int k=0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] != 0) ans.push_back(nums[i]);else c++;
+            if(nums[i] != 0){
+                nums[k++] = nums[i];
+            }
         }
-        for(int i=0 ;i <c; i++){
-            ans.push_back(0);
+        for(int i=k; i<nums.size();i++){
+            nums[i]=0;
         }
-        return ans;
+        
+        return nums;
     }
 };

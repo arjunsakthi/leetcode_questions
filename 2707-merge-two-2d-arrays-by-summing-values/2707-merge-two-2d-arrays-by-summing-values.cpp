18bf759c9334ec28a76e6ff5f98569539dfa0cdf
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-02 16:04:13 +0530

    Time: 0 ms (100%), Space: 14.8 MB (44.73%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,17 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        map<int,int> mp;
-        for(int i=0; i<nums1.size(); i++){
-            mp[nums1[i][0]]+=nums1[i][1];
-        }
-        
-        for(int i=0; i<nums2.size(); i++){
-            mp[nums2[i][0]]+=nums2[i][1];
-        }
         vector<vector<int>> ans;
-        for(auto i:mp){
-            ans.push_back({i.first,i.second});
+        int i1=0,i2=0;
+       
+        if(nums1[0][0] < nums2[0][0]){
+ ans.push_back({nums1[0][0],nums1[0][1]});
+        i1++;
+        }
+        else{
+             ans.push_back({nums2[0][0],nums2[0][1]});
+        i2++;
+        }
+        while(i1 < nums1.size() && i2<nums2.size()){
+            if(nums1[i1][0] < nums2[i2][0]){
+                if(ans.back()[0] == nums1[i1][0]){
+                    ans[ans.size()-1][1] += nums1[i1][1];
+                    
+                }
+                else{
+                    ans.push_back({nums1[i1][0],nums1[i1][1]});
+                }
+                i1++;
+            }
+            else{
+                if(ans.back()[0] == nums2[i2][0]){
+                    ans[ans.size()-1][1] += nums2[i2][1];
+                }
+                else{
+                    ans.push_back({nums2[i2][0],nums2[i2][1]});
+                }
+                i2++;
+            }
+        }
+        while(i1<nums1.size()){
+             if(ans.back()[0] == nums1[i1][0]){
+                    ans.back()[1] += nums1[i1][1];
+                    
+                }
+                else{
+                    ans.push_back({nums1[i1][0],nums1[i1][1]});
+                }
+                i1++;
+        }
+         while(i2<nums2.size()){
+              if(ans.back()[0] == nums2[i2][0]){
+                    ans.back()[1] += nums2[i2][1];
+                }
+                else{
+                    ans.push_back({nums2[i2][0],nums2[i2][1]});
+                }
+                i2++;
         }
         return ans;
     }

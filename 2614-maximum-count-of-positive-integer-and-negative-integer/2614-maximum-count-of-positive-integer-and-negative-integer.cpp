08586f25441004196fa90497ae1d2c19024f1678
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-12 12:06:23 +0530

    Time: 0 ms (100%), Space: 21.2 MB (99.87%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -6,12 +6,11 @@ public:
         auto pos = upper_bound(nums.begin(),nums.end(),0);
         int negative =0;
         int positive=0;
-        if(neg != nums.begin()){
             negative = neg - nums.begin();
-        }
-        if(pos != nums.end()){
+        
+     
             positive = nums.end() - pos;
-        }
+    
         return max(positive,negative);
     }
 };

fd5e40ff37d4572b56170460cabb354e8c1e4990
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-13 11:49:18 +0530

    Time: 122 ms (78.38%), Space: 50.2 MB (38.34%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,28 +1,77 @@
+// standard method n - n+1
+
+// class Solution {
+// public:
+//     long long countOfSubstrings(string word, int k) {
+//         return kvowel(word,k)-kvowel(word,k+1);
+//     }
+// private :
+//     long long kvowel(string &word, int k){
+//         long long sum=0;
+//         int cons=0;
+//         vector<int> vowel(26,0);
+        
+//         int l=0, r=0;
+//         int n=word.size();
+//         while(r<n){
+//             vowel[word[r]-'a']++;
+//             cons += !checkVowel(word[r]);
+//             while(checkWind(vowel) && cons>=k){
+//                 sum += n-r;
+//                 vowel[word[l]-'a']--;
+//                 cons -= !checkVowel(word[l]);
+//                 l++;
+//             }
+//             r++;
+//         } 
+//         return sum;
+//     }
+//     bool checkVowel(char &c){
+//         return (c=='a' || c=='e' || c=='i' || c=='o'|| c=='u');
+//     }
+//     bool checkWind(vector<int> &charWind){
+//         return (charWind['a'-'a'] && charWind['e'-'a'] && charWind['i'-'a'] && charWind['o'-'a'] && charWind['u'-'a'] );
+//     }
+// };
+
+// trying with precomputation
+
 class Solution {
 public:
     long long countOfSubstrings(string word, int k) {
-        return kvowel(word,k)-kvowel(word,k+1);
-    }
-private :
-    long long kvowel(string &word, int k){
         long long sum=0;
         int cons=0;
+        int n =word.size();
         vector<int> vowel(26,0);
+        vector<int> nextC(n,n);
+        int ind = n;
+        for(int i=n-1; i>=0; i--){
+            nextC[i] = ind;
+            if(!checkVowel(word[i])){
+                ind = i;
+            }
+        }
         int l=0, r=0;
-        int n=word.size();
         while(r<n){
             vowel[word[r]-'a']++;
             cons += !checkVowel(word[r]);
-            while(checkWind(vowel) && cons>=k){
-                sum += n-r;
+            while(checkWind(vowel) && cons>k){
                 vowel[word[l]-'a']--;
                 cons -= !checkVowel(word[l]);
                 l++;
             }
+            while(checkWind(vowel) && cons==k){
+                sum += nextC[r]-r;
+                vowel[word[l]-'a']--;
+                cons -= !checkVowel(word[l]);
+                l++;
+            }
+            
             r++;
         } 
         return sum;
     }
+private :
     bool checkVowel(char &c){
         return (c=='a' || c=='e' || c=='i' || c=='o'|| c=='u');
     }

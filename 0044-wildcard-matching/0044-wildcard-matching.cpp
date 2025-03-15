45cda6680bf37764b9ba8a87418000322df8abfd
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-15 19:05:53 +0530

    Time: 28 ms (70.9%), Space: 31.6 MB (25.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-    bool isAllStars(string &S1, int i) {
-    // S1 is taken in 1-based indexing
-    for (int j = 1; j <= i; j++) {
-        if (S1[j - 1] != '*')
-            return false;
-    }
-    return true;
-}
-    bool rec(int i, int j, string s, string p){
-        if(i<0 && j<0)  return true;
-        else if(i <0 || j<0)    return false;
-
-        if(p[j] == '?'){
-            return rec(i-1,j-1,s,p);
+    bool rec(string &s, string &p, int i, int j,vector<vector<int>> &dp){
+        if(j<0 && i<0)  return true;
+        if(j<0) return false;
+          
+        if(i<0){
+            while(j>=0 && p[j]=='*'){
+                j--;
+            }
+            return j==-1?true:false;
         }
-        else if(p[j] == '*'){
-
-                return rec(i-1,j,s,p) || rec(i,j-1,s,p) || rec(i-1,j-1,s,p);
+      if(dp[i][j] != -1)return dp[i][j];
+        if(s[i]==p[j] || p[j] == '?'){
+            return dp[i][j] = rec(s,p,i-1,j-1,dp);
         }
         else{
-            if(s[i] != p[j])    return false;
-            else    return rec(i-1,j-1,s,p);
-        }
-        return false;
-    }
-    bool isMatch(string s, string p) {
-        // return rec(s.size()-1, p.size()-1, s, p);
-        vector<vector<bool>> dp(s.size()+1, vector<bool>(p.size()+1,0));
-        dp[0][0] = true;
-        for (int i = 1; i <= p.size(); i++) {
-            dp[0][i] = isAllStars(p, i);
-        }
-        for(int i=1; i<=s.size(); i++){
-            for(int j=1; j<=p.size(); j++){
-                if(p[j-1] == '?'){
-            dp[i][j] = dp[i-1][j-1];
-        }
-        else if(p[j-1] == '*'){
-
-                dp[i][j] = dp[i-1][j] || dp[i][j-1];
-        }
-        else{
-            dp[i][j] = (s[i - 1] == p[j - 1]) && dp[i - 1][j - 1];
-        }
+            if(p[j] == '*'){
+                return dp[i][j] = (rec(s,p,i-1,j,dp)||rec(s,p,i,j-1,dp));
+            }
+            else{
+                return dp[i][j] = false;
             }
         }
-        return dp[s.size()][p.size()];
+    }
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
+        return rec(s,p,s.size()-1,p.size()-1,dp);
     }
 };

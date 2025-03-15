713c414dccc135ffd315a03d7d10dedabb676959
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-15 11:51:14 +0530

    Time: 21 ms (70.94%), Space: 60.9 MB (67.69%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool whetherCanRob(vector<int> &nums, int &k, int mini){
-        vector<vector<int>> dp(nums.size()+1, vector<int>(2,0));
-        for(int i=1; i<=nums.size(); i++){
-            for(int j=0; j<=1; j++){
-                if(j==0){
-                    dp[i][j] = max((nums[i-1] <= mini ?1:0)+dp[i-1][1], dp[i-1][0]);
-                }
-                else{
-                    dp[i][j] = dp[i-1][0];
-                }
+        int count =0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] <= mini){
+                i++;
+                count++;
             }
         }
-        cout << max(dp[nums.size()][0],dp[nums.size()][1]) << " " << mini<<endl;
-        return max(dp[nums.size()][0],dp[nums.size()][1]) >= k;
+        return count >= k;
     }
     int minCapability(vector<int>& nums, int k) {
         int low =0;

75aa65cd9cc037c7aeeef51d706f028463ca0687
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-17 21:35:36 +0530

    Time: 16 ms (8.76%), Space: 124.8 MB (38.04%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,35 +10,15 @@
  */
 class Solution {
 public:
+    ListNode* palind(ListNode* head, ListNode* current, int &ans){
+        if(current == NULL) return head;
+        ListNode* comp = palind(head,current->next,ans);
+        if(comp->val != current->val) ans = 0;
+        return comp->next;
+    }
     bool isPalindrome(ListNode* head) {
-        string num = "";
-        int count = 0;
-        if(head == NULL && head->next == NULL){
-            return true;
-        }
-        ListNode* fast = head->next;
-        ListNode* slow = head;
-        while(fast != NULL && fast->next != NULL){
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        if(fast == NULL){
-            slow = slow->next;
-        }
-        ListNode *prev = NULL;
-        ListNode *nxt = NULL;
-        while(slow != NULL){
-            nxt = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nxt;
-        }
-        while(prev != NULL && head != NULL){
-            if(prev->val != head->val)  return false;
-            prev = prev->next;
-            head = head->next;
-        }
-        
-        return true;
+        int ans=1;
+        palind(head, head,ans);
+        return ans;
     }
 };

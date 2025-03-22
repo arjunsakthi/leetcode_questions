6b58c373a7b42bec5c0c111ced4ab782a0b2146f
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-22 12:37:13 +0530

    Time: 1 ms (45.98%), Space: 60.9 MB (61%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -3,22 +3,16 @@ public:
     int longestNiceSubarray(vector<int>& nums) {
         int maxi = 1;
         int low=0,high=0;
-        int sum=0;
-        int exor=0;
+        int usedBits = 0;
         while(high <nums.size()){
-            if(sum == exor){
-                maxi = max(high - low,maxi);
-                exor ^= nums[high];
-                sum += nums[high];
-                high++;
-            }
-            else{
-                exor ^= nums[low];
-                sum -= nums[low];
+            while((usedBits & nums[high])!=0){
+                usedBits ^= nums[low];
                 low++;
             }
+            usedBits ^= nums[high];
+            maxi = max(maxi, high - low+1);
+            high++;
         } 
-        if(sum == exor)maxi = max(high - low,maxi);
         return maxi;
     }
 };

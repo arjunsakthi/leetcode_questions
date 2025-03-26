b43e48f06f2b7ae8b68ef362213ecfbc737a71c8
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-03-26 19:49:14 +0530

    Time: 155 ms (88.03%), Space: 132.5 MB (14.83%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int seive(int n){
+        vector<int> seive(n,0);
+        int c=0;
+        for(int i=3; i<n; i+=2){
+            if(!seive[i]){
+                c++;
+                for(int j=i; j<n; j+=i){
+                    seive[j]=1;
+                }
+            }
+        }
+        return n>2 ? c+1 : c;
+    }
+    int countPrimes(int n) {
+        return seive(n);
+    }
+};

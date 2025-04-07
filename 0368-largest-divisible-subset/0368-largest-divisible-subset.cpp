b67372b6fd9ba5b3806e1678fa8a58bb0238a3a8
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-04-07 07:51:03 +0530

    Time: 17 ms (20.41%), Space: 12.6 MB (46.78%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,1);
-        int lastIndex = 0;
-        vector<int> hash(n,1);
-        sort(nums.begin(),nums.end());
-        int maxi = 1;
-        for(int i=0; i<n; i++){
-            hash[i] = i;
-            for(int j=0; j<i; j++){
-                if(nums[i]%nums[j] == 0 && dp[j]+1 > dp[i]){
-                    dp[i] = dp[j]+1;
-                    hash[i]=j;
-                }
-            }
-            if(maxi < dp[i]){
-                maxi = dp[i];
-                lastIndex=i;
-            }
-        }
+        int maxInd = 0;
         vector<int> ans;
-        while(hash[lastIndex] != lastIndex){
-            ans.push_back(nums[lastIndex]);
-            lastIndex = hash[lastIndex];
+        sort(nums.begin(),nums.end());
+       vector<int> dp(nums.size(),1), parent(nums.size(),0);
+       dp[0] = 1;
+       for(int i=0; i<nums.size(); i++){
+            parent[i] = i;
+       }
+       for(int i=1;i<nums.size();i++){
+        for(int j=0; j<i; j++){
+            if((nums[i]%nums[j]==0 || nums[j]%nums[i] == 0) && 1+dp[j] > dp[i]){
+                dp[i] = 1+dp[j];
+                parent[i]=j;
+            }
         }
-        ans.push_back(nums[lastIndex]);
-        return ans;
+       }
+       int maxi = 0;
+       for(int i=0; i<nums.size(); i++){
+        if(dp[i] > maxi){
+            maxInd = i;
+            maxi = dp[i];
+        }
+       }
+       ans.push_back(nums[maxInd]);
+       while(maxInd != parent[maxInd]){
+            maxInd = parent[maxInd];
+            ans.push_back(nums[maxInd]);
+       }
+       return ans;
+
     }
 };

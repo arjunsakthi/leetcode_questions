0b6c1426f0690621ed3a3afdf32173e8ece18638
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-04-07 09:24:22 +0530

    Time: 184 ms (15.28%), Space: 17.1 MB (62.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    bool f_rec(vector<int>& nums, double temp_sum, int n, double sum,vector<vector<double>>& dp){
-        if(temp_sum > sum/2)    return false;
-        if(temp_sum == sum/2)   return true;
-        if(n == 0)  return (nums[0]+temp_sum == sum/2);
-        if(dp[n][temp_sum] != -1)  return dp[n][temp_sum];
-        int take = f_rec(nums, temp_sum + nums[n], n-1, sum, dp);
-        int nottake = f_rec(nums, temp_sum,n-1,sum, dp);
-        return dp[n][temp_sum] = take || nottake;
-    }
-
     bool canPartition(vector<int>& nums) {
-        double sum = 0;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum%2)   return false;
+        int half = sum/2;
+        vector<vector<bool>> dp(nums.size(),vector<bool>(half+1,0));
+        for(int i=0; i<nums.size(); i++)    dp[i][0] = 1;
+        if(nums[0] <= half)dp[0][nums[0]]=1;
+        for(int i=1; i<nums.size(); i++){
+            for(int j=0; j<=half; j++){
+                 dp[i][j] = dp[i-1][j] ;
+                if(j - nums[i]>=0){
+                    dp[i][j] = dp[i][j] |  dp[i-1][j-nums[i]];
+                }
+            }
         }
-        double temp_sum = 0;
-        vector<vector<double>> dp(nums.size()+1,vector<double>(sum+1,-1));
-        bool k = f_rec(nums, temp_sum, nums.size()-1, sum, dp);
-        cout << sum << temp_sum;
-        return k;
+        return dp[nums.size()-1][half];
     }
 };

7c7ab04e7e8a06c10c9ddf7886b6da4776b3e40d
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-04-13 18:48:20 +0530

    Time: 2192 ms (54.29%), Space: 100.4 MB (71.43%) - LeetHub

--- 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
@@ -1,51 +1,21 @@
-
 class Solution {
 public:
-    int uniqueXorTriplets(vector<int>& nums) {
-        const int S = 2048;
-        int n = nums.size();
-        
-        // Rolling DP: two 2D arrays
-        vector<vector<int>> prev(S, vector<int>(3, 0));
-        vector<vector<int>> curr(S, vector<int>(3, 0));
-        
-        // Base initialization at index 0 (mimicking your original base)
-        prev[0][1] = 1;             // dp[0][0][1] = 1
-        prev[nums[0]][0] = 1;         // dp[0][nums[0]][0] = 1
-        prev[nums[0]][2] = 1;         // dp[0][nums[0]][2] = 1
-        
-        // Process from i = 1 to n-1
-        for (int i = 1; i < n; i++) {
-            // Clear current DP array
-            for (int j = 0; j < S; j++) {
-                curr[j][0] = curr[j][1] = curr[j][2] = 0;
+    int uniqueXorTriplets(vector<int>& a) {
+        unordered_set<int> st, s;
+        int n = a.size();
+        if(n==1 || n==2) return n;
+
+        for(int i=0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                st.insert(a[i]^a[j]);
             }
-            
-            for (int j = 0; j < S; j++) {
-                // Compute XOR index with current element
-                int exor = (j ^ nums[i]) & 2047;
-                
-                // Transition for state 0
-                if (j == nums[i]) curr[j][0] |= 1;  // If current XOR equals nums[i]
-                curr[j][0] |= prev[j][0];
-                
-                // Transition for state 1
-                curr[j][1] |= prev[j][1];
-                curr[j][1] |= prev[exor][0];
-                
-                // Transition for state 2
-                curr[j][2] |= prev[j][2];
-                curr[j][2] |= prev[exor][1];
-                curr[j][2] |= prev[j][0];
+        }
+
+        for(auto num: st){
+            for(auto x: a){              
+                s.insert(num^x);          
             }
-            // Roll current DP to previous DP for next iteration
-            prev.swap(curr);
         }
-        
-        int sum = 0;
-        for (int j = 0; j < S; j++) {
-            if (prev[j][2]) sum++;
-        }
-        return sum;
+        return s.size();
     }
 };

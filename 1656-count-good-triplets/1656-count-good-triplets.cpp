fa9c27536f2d167af0574f5ccdb9c4cf5db95233
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2025-04-14 10:25:13 +0530

    Time: 16 ms (5.06%), Space: 11.1 MB (43.88%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int count = 0;
-        for(int i=0; i<arr.size()-2; i++){
-            for(int j=i+1; j<arr.size()-1; j++){
+        multiset<int> ms;
+
+            for(int j=1; j<arr.size()-1; j++){
+                ms.insert(arr[j-1]);
                 for(int k=j+1; k<arr.size(); k++){
-                    if(abs(arr[i]-arr[j]) <= a && abs(arr[i]-arr[k]) <= c && abs(arr[j]-arr[k]) <= b)   count++;
+                    int high = min(a + arr[j], c + arr[k]);
+                    int low = max(arr[j] - a, arr[k] - c);
+                    if(high < low)  continue;
+                    int lower = distance(ms.begin(), ms.lower_bound( low));
+                    int upper = distance(ms.begin(), ms.upper_bound( high));
+                    if(abs(arr[j]-arr[k]) <= b)
+                        count += upper - lower;
                 }
             }
-        }
+        
         return count;
     }
 };

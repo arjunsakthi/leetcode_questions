7b35571acb6cf50d2126fa9b863d2fe3e98a14ed
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2026-01-14 18:56:00 +0530

    Time: 4 ms (46.36%), Space: 23.6 MB (81.45%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int,int> mp;
-        for(auto i: nums){
-            mp[i]++;
+        int cnt1=0,cnt2=0,el1,el2;
+        cout << el1 << el2 << endl;
+        for(int i=0; i<nums.size();i++){
+            if(cnt1==0 && el2 != nums[i]) {
+                el1=nums[i];
+                cnt1++;
+            }
+            else if(cnt2==0 && el1 != nums[i]) {
+                el2=nums[i];
+                cnt2++;
+            }
+            else if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
+            else {cnt1--;cnt2--;}
         }
-        for(auto i: mp){
-            if(i.second > nums.size()/3)    ans.push_back(i.first);
+        int c1=0, c2=0; 
+        cout << el1 << el2 << cnt1 << cnt2;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i]==el1) c1++;
+            if(nums[i]==el2) c2++;
         }
-        return ans;
+        if(c1>nums.size()/3 && c2>nums.size()/3) {
+            return {el1,el2};
+        }
+        else if(c1>nums.size()/3) {
+            return {el1};
+        }
+        else if(c2>nums.size()/3) {
+            return {el2};
+        }
+        else return {};
     }
 };

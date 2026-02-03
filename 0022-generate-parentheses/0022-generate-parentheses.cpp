a5cb46ea5c3646882d4b18e836505828a656d310
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2026-02-03 23:38:55 +0530

    Time: 2 ms (81.96%), Space: 13.2 MB (79.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,11 +3,11 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string s;
-        fun(s, n, n, ans);
+        fun(s, n, n, ans, n);
         return ans;
     }
 
-    void fun(string& s, int open, int close, auto& ans){
+    void fun(string& s, int open, int close, auto& ans, int n){
         if(open==0 && close==0) {
             ans.push_back(s);
             return ;
@@ -17,10 +17,10 @@ public:
         if(close<open) return;
 
         s.push_back('(');
-        fun(s, open-1, close, ans);
+        fun(s, open-1, close, ans, n);
         s.pop_back();
         s.push_back(')');
-        fun(s, open, close-1, ans);
+        if (close > open) fun(s, open, close-1, ans, n);
         s.pop_back();
     }
 };

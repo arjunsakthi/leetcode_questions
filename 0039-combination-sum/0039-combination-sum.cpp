7fcb038993f8b57890dae8972c6b52018d6a8e90
Sakthivel Karthikeyan <75869725+arjunsakthi@users.noreply.github.com>
2026-02-05 18:25:42 +0530

    Time: 15 ms (20.12%), Space: 18.5 MB (20.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void f_rec(vector<vector<int>>& ans, vector<int> temp, vector<int>& candidates,int target, int k){
-        if(target == 0 || k == candidates.size()){
-            if(target == 0){
-                ans.push_back(temp);
-            }
-            return;
+    void f(vector<int>& cand, int i, int target, vector<int>& temp, set<vector<int>>& ans) {
+        
+        if(target == 0) {
+            ans.insert(temp);
         }
-        if(candidates[k] > target) return;
-        temp.push_back(candidates[k]);
-        f_rec(ans, temp, candidates, target - candidates[k], k);
+        if(target < 0 || i == cand.size()) return;
+
+        f(cand, i+1, target, temp, ans);
+        int a = cand[i];
+        temp.push_back(a);
+        f(cand, i+1, target-cand[i], temp, ans);
+        f(cand, i, target-cand[i], temp, ans);
         temp.pop_back();
-        f_rec(ans, temp, candidates, target, k+1);
         return;
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        sort(candidates.begin(), candidates.end());
+        int i=0;
         vector<int> temp;
-        f_rec(ans, temp, candidates, target, 0);
-        return ans;
+        set<vector<int>> ans;
+        f(candidates,i,target,temp,ans);
+        return vector<vector<int>>(ans.begin(), ans.end());;
     }
 };
